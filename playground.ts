72b66330bf9f6df527e6bebf8594baedb7f01e73
Alex Clarke <clarkexcore@gmail.com>
2022-04-22 16:38:20 -0400

    somne more function types

--- playground.ts
+++ playground.ts
@@ -1,37 +1,25 @@
-// TS Recommends interfaces over types when possible
-
-// They are very similar
-
-// interfaces describe the data structures in a more natural way
-// interface describing objects
-
 interface Person {
-    name: string,
-    age: number
+	name: string;
+	age: number;
 }
 
-
-// When to use types, is when you need to create type aliases
-// to describe function types
-
-
 // interface PersonLoggerFn {
-//     logPersonInfo(): string
+// 	(person: Person): string;
 // }
 
-// type Data = string
+type PersonLoggerFn = (person: Person) => string;
 
-export default function play(){
-    const person = {
-        name: "Alex",
-        age: 31
-    }
+export default function play() {
+	const person: Person = {
+		name: "Alex",
+		age: 31,
+	};
 
-    function logPersonInfo(person: Person){
-        const info = `Name: ${person.name}, Age: ${person.age}`;
-        console.log(info);
-        return info;
-    }
+	const logPersonInfo: PersonLoggerFn = (person: Person): string => {
+		const info = `Name: ${person.name}, Age: ${person.age}`;
+		console.log(info);
+		return info;
+	};
 
-    logPersonInfo(person)
+	const log: string = logPersonInfo(person);
 }
